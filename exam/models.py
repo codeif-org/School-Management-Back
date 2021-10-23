19610902d5302a88d83359d2344499e1ce540087
Mayur Gupta <mayurgupta7234@gmail.com>
2021-10-23 13:04:30 +0530

    build exam model

--- exam/models.py
+++ exam/models.py
@@ -1,14 +1,16 @@
 from django.db import models
 from teacher.models import teacher, classSection, subject
 from student.models import student
+
 # Create your models here.
 class exam(models.Model):
-    teacher = models.ForeignKey(teacher, on_delete=models.PROTECT)
+    # teacher = models.ForeignKey(teacher, on_delete=models.PROTECT)
     classSection = models.ForeignKey(classSection, on_delete=models.PROTECT)
     subject = models.ForeignKey(subject, on_delete=models.PROTECT)
     date = models.DateField(auto_now=True)
     name = models.CharField(max_length=100)
-    marks = models.IntegerField()
+    max_marks = models.IntegerField()
+    ms = models.BooleanField()
 
 class score(models.Model):
     exam = models.ForeignKey(exam, on_delete=models.PROTECT)
