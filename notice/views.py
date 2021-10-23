3e77c182d3588764c216e463e50ba76aca34cb1b
Mansi <mansigarg2280@gmail.com>
2021-10-23 14:50:11 -0700

    Notice Done!

--- notice/views.py
+++ notice/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render
-from .models import notice
+from django.shortcuts import render, redirect
+from .models import notice, receiver
 from teacher.models import classSection
 from student.models import student
 # Create your views here.
 
 def showNotice(request):
-    notices = notice.objects.all()
-    return render(request, 'addNotice.html', {'notices': notices})
+    notices = notice.objects.all().order_by('-date')
+    receivers = receiver.objects.all()
+    return render(request, 'addNotice.html', {'notices': notices, 'receivers': receivers})
 
 def createNotice(request):
     classes = classSection.objects.all()
@@ -15,4 +16,21 @@ def createNotice(request):
     l = []
     for i in range(1, number+1):
         l.append(i)
+    if request.method == 'POST':
+        topic = request.POST['topic']
+        desc = request.POST['desc']
+        noticeobj = notice(topic = topic, desc = desc)
+        noticeobj.save()
+        allclasses = request.POST.getlist('checks[]')
+        print(allclasses)
+        if 'all' in allclasses:
+            for c in classes:
+                receiverobj = receiver(note = noticeobj, receiver = c)
+                receiverobj.save()
+        else:
+            for c in allclasses:
+                classobj = classSection.objects.get(Class = c)
+                receiverobj = receiver(note = noticeobj, receiver = classobj)
+                receiverobj.save()
+        return redirect('notice:showNotice')
     return render(request, 'createNotice.html', {'classes': classes, 'students': students, 'l': l})
