06e7e22d45545472164d64833a3e1aa7fc3d9061
Mayur Gupta <mayurgupta7234@gmail.com>
2021-10-25 21:51:18 +0530

    update saveAttendance API

--- attendance/urls.py
+++ attendance/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = "attendance"
 urlpatterns = [
     path('teacher/', views.Attendance, name='attendance'),
-    path('teacher/api/saveAttendance/<int:student_id>', views.saveAttendance, name='saveAttendance'),
+    path('teacher/api/saveAttendance/', views.saveAttendance, name='saveAttendance'),
 ]
--- attendance/views.py
+++ attendance/views.py
@@ -2,42 +2,58 @@ from django.shortcuts import render
 from teacher.models import teacher, classSection
 from student.models import student
 from attendance.models import attendance
+
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import attendanceSerializer
 from django.http.response import JsonResponse
-from rest_framework.parsers import JSONParser 
+from rest_framework.parsers import JSONParser
 from rest_framework import serializers, status
 import datetime
 # Create your views here.
 
 
-@api_view(['POST', 'PATCH'])
-def saveAttendance(request, student_id):
+@api_view(['GET', 'POST', 'PATCH'])
+def saveAttendance(request):
     print("123456789")
-    stu = student.objects.get(id = student_id)
-    print(stu.fname)
+    # student_id = request.data['student']
+    # stu = student.objects.get(id=student_id)
+    print(request.GET)
+    # print(stu.fname)
+    if request.method == "GET":
+        if "student" in request.GET:
+            stu = request.GET["student"]
+            att = attendance.objects.filter(student=stu)
+            print(att)
+            serializer = attendanceSerializer(att, many=True)
+            return Response(serializer.data)
+        return Response({"msg": "Add query parameter 'student' against URL"})
+
     if request.method == 'POST':
-        request.data['student'] = stu.id
+        # request.data['student'] = stu.id
         attendance_serializer = attendanceSerializer(data=request.data)
         if attendance_serializer.is_valid():
             attendance_serializer.save()
-            return JsonResponse(attendance_serializer.data, status=status.HTTP_201_CREATED) 
+            return JsonResponse(attendance_serializer.data, status=status.HTTP_201_CREATED)
         return JsonResponse(attendance_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     elif request.method == 'PATCH':
         date = request.data['date']
-        obj = attendance.objects.get(student = stu, date = date)
-        attendance_serializer = attendanceSerializer(obj, data = request.data, partial = True)
+        stu = request.data['student']
+        obj = attendance.objects.get(student=stu, date=date)
+        attendance_serializer = attendanceSerializer(
+            obj, data=request.data, partial=True)
         if attendance_serializer.is_valid():
             attendance_serializer.save()
-            return JsonResponse(attendance_serializer.data, status=status.HTTP_201_CREATED) 
+            return JsonResponse(attendance_serializer.data, status=status.HTTP_201_CREATED)
         return JsonResponse(attendance_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 def Attendance(request):
     user = request.user
-    t = teacher.objects.get(username = user.username)
-    Class = classSection.objects.get(teacher = t)
-    students = student.objects.filter(school = t.school, Class = Class)
+    t = teacher.objects.get(username=user.username)
+    Class = classSection.objects.get(teacher=t)
+    students = student.objects.filter(school=t.school, Class=Class)
     number = len(students)
     l = []
     for i in range(1, number+1):
