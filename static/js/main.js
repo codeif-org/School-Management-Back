b3f7b3482b51e5092e3b4c5f03e87dc377ceb123
Mayur Gupta <mayurgupta7234@gmail.com>
2021-10-26 10:01:09 +0530

    added main.js for attendance btns

--- static/js/main.js
+++ static/js/main.js
@@ -1,16 +1,52 @@
-console.log("main js added")
+console.log("main js added");
 
-let elems = document.querySelectorAll('.att-student-list button')
-elems.forEach((elem) => {
-    elem.addEventListener('click', (event) => {
-        clickHandler(event)
-    })
-})
+// var elems = document.querySelectorAll(".att-student-list button");
+// var elems = document.getElementsByClassName("att-btn")
+// console.log(elems)
+// elems.forEach((elem) => {
+//   elem.addEventListener("click", (event) => {
+//     clickHandler(event);
+//   });
+// });
 
-function clickHandler(event) {
-    // console.log("clicked")
-    // console.log(event.target)
-    console.log(event.target.tag)
-    event.target.classList.toggle("btn-clicked")
+// var clickHandler = function (event) {
+//   // console.log("clicked")
+//   // console.log(event.target)
+//   event.target.classList.toggle("btn-clicked");
 
-}
+//   console.log(event.target);
+//   let elem = event.target;
+//   if (elem.parentNode.childNodes[3] === elem) {
+//     console.log("print here");
+//     toggle(elem.parentNode.childNodes[1]);
+//   }
+
+//   // console.log(elem.parentNode.childNodes[3])
+//   // event.target.classList.toggle("btn-clicked")
+// };
+
+// var toggle = function (eleme) {
+//   console.log("here eleme", eleme);
+//   console.log(!("btn-clicked" in eleme.classList))
+// //   if (eleme.classList.includes("btn-clicked")) {
+// //     console.log("yes");
+// //   }
+// };
+
+var btnHandler = function (event) {
+  //   console.log(event);
+  var elem = event.target;
+  console.log(elem);
+  elem.classList.toggle("btn-clicked");
+  if (elem.previousElementSibling) {
+    if (elem.previousElementSibling.classList.contains("btn-clicked")) {
+      elem.previousElementSibling.classList.toggle("btn-clicked");
+    }
+  } else {
+    if (elem.nextElementSibling) {
+      if (elem.nextElementSibling.classList.contains("btn-clicked")) {
+        elem.nextElementSibling.classList.toggle("btn-clicked");
+      }
+    }
+  }
+};
