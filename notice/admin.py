3a162126a0c71c0997b8737d26246a11982b4e8f
Mayur Gupta <mayurgupta7234@gmail.com>
2021-11-09 01:10:43 +0530

    updated noticeAdmin

--- notice/admin.py
+++ notice/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from .models import notice, receiver
 # Register your models here.
-# @admin.register(notice)
-# class noticeAdmin(admin.ModelAdmin):
-#     list_display = ('topic', 'desc', 'date')
-admin.site.register(notice)
+
+@admin.register(notice)
+class noticeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'date', 'topic', 'desc')
+# admin.site.register(notice)
 
 @admin.register(receiver)
 class receiverAdmin(admin.ModelAdmin):
