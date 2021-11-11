3d9fe295310c5f3dc9148d40bc414a908c3b3e39
Mayur Gupta <mayurgupta7234@gmail.com>
2021-11-11 12:15:16 +0530

    created new leaderboard

--- exam/urls.py
+++ exam/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('marksEdit/<int:id>', views.marksEdit, name='marksEdit'),
     path('createExam/', views.createExam, name='createExam'),
     path('api/marksupdate/', views.marksUpdate, name='marksUpdate'),
-    path('student/leaderboard/<int:subject_id>', views.leaderboard, name='leaderboard'),
+    path('student/leaderboard/', views.leaderboard, name='leaderboard'),
     path('student/progress/<int:subject_id>', views.progress, name='progress'),
 ]
--- exam/views.py
+++ exam/views.py
@@ -102,29 +102,29 @@ def marksUpdate(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-def leaderboard(request, subject_id):
-    studentobj = student.objects.get(user = request.user)
-    classobj = studentobj.Class
-    print(classobj)
-    subjects = subject.objects.filter(Class = classobj)
-    print(subjects)
-    exams = ExamHeldSubject.objects.filter(subject = subject.objects.get(id = subject_id))
-    print("exams:", exams)
-    students = student.objects.filter(Class = classobj)
-    # print(students)
-    marks = []
-    for s in students:
-        m = 0
-        for e in exams:
-            # print(e, s)
-            try:
-                mark = score.objects.get(exam_held = e, stu = s)
-                m = m + mark.score
-            except:
-                m = 0
-        marks.append(m)
-    # print(marks)
-    return render(request, 'leaderboard.html', {'subjects': subjects, 'sub': subjects[0], 'students': students, 'class': classobj, 'marks': marks})
+# def leaderboard(request, subject_id):
+#     studentobj = student.objects.get(user = request.user)
+#     classobj = studentobj.Class
+#     print(classobj)
+#     subjects = subject.objects.filter(Class = classobj)
+#     print(subjects)
+#     exams = ExamHeldSubject.objects.filter(subject = subject.objects.get(id = subject_id))
+#     print("exams:", exams)
+#     students = student.objects.filter(Class = classobj)
+#     # print(students)
+#     marks = []
+#     for s in students:
+#         m = 0
+#         for e in exams:
+#             # print(e, s)
+#             try:
+#                 mark = score.objects.get(exam_held = e, stu = s)
+#                 m = m + mark.score
+#             except:
+#                 m = 0
+#         marks.append(m)
+#     # print(marks)
+#     return render(request, 'leaderboard.html', {'subjects': subjects, 'sub': subjects[0], 'students': students, 'class': classobj, 'marks': marks})
 
 def progress(request, subject_id):
     studentobj = student.objects.get(user = request.user)
@@ -139,3 +139,8 @@ def progress(request, subject_id):
         scores.append(scoreobj)
     print(scores)
     return render(request, 'progress.html', {'scores': scores, 'subjects': subjects})
+
+
+def leaderboard(request):
+    print(request.user.id)
+    return render(request, 'leaderboard.html')
