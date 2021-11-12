6ba57d4bde80b8de1804bbe4908371459feb54f6
Mayur Gupta <mayurgupta7234@gmail.com>
2021-11-12 09:06:59 +0530

    updated leaderboard.html done html work

--- static/js/leaderboard.js
+++ static/js/leaderboard.js
@@ -1,1 +1,8 @@
 console.log('leaderboard.js');
+
+let submitHandler = function () {
+    console.log('submitHandler');
+    let exam = document.getElementById('examselect').value;
+    let subject = document.getElementById('subselect').value;
+    console.log(exam, subject);
+}
