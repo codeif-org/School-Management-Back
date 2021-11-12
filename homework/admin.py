1d08cecf1f57e6c0d7fff7d6b9f3cccf028792e8
Mansi <mansigarg2280@gmail.com>
2021-11-12 13:55:09 +0530

    HomeworkSubmissionStarted!

--- homework/admin.py
+++ homework/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Homework, Student_Homework
+from .models import Homework, Student_Homework, HomeworkSubmission
 # Register your models here.
 @admin.register(Homework)
 class HomeworkAdmin(admin.ModelAdmin):
@@ -8,3 +8,7 @@ class HomeworkAdmin(admin.ModelAdmin):
 @admin.register(Student_Homework)
 class Student_HomeworkAdmin(admin.ModelAdmin):
     list_display = ('subject', 'homework', 'Class')
+
+@admin.register(HomeworkSubmission)
+class HomeworkSubmissionAdmin(admin.ModelAdmin):
+    list_display = ('student', 'homework', 'sub_date')
--- homework/models.py
+++ homework/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from student.models import student
 from teacher.models import classSection, subject,  teacher
 
 # Create your models here.
@@ -13,3 +13,8 @@ class Student_Homework(models.Model):
     subject = models.ForeignKey(subject, on_delete=models.CASCADE)
     homework = models.ForeignKey(Homework, on_delete=models.CASCADE)
     Class = models.ForeignKey(classSection, on_delete=models.CASCADE)
+
+class HomeworkSubmission(models.Model):
+    homework = models.ForeignKey(Homework, on_delete=models.CASCADE)
+    student = models.ForeignKey(student, on_delete=models.CASCADE)
+    sub_date = models.DateTimeField(auto_now=True)
