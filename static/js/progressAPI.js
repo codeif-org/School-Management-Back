0d38a4719b278bf351978e9e9c22ba0cb0e99f6b
Mayur Gupta <mayurgupta7234@gmail.com>
2021-11-15 03:40:07 +0530

    added chart_func in progressAPI.js

--- static/js/progressAPI.js
+++ static/js/progressAPI.js
@@ -74,10 +74,55 @@ console.log(mydata);
 console.log(typeof mydata);
 
 let mydata_arr = Object.entries(mydata);
+let labels_data = [];
+let percent_data = [];
 mydata_arr.forEach((elem) => {
-    console.log(elem);
+    // console.log(elem);
+    labels_data.push(elem[1][0]);
+    percent_data.push(elem[1][3]);
 })
 
+console.log(labels_data);
+console.log(percent_data);
 
 
+let chart_func = function (labels_data, percent_data) {
+var ctx = document.getElementById('myChart').getContext('2d');
+  var myChart = new Chart(ctx, {
+      type: 'pie',
+      data: {
+          labels: labels_data,
+          datasets: [{
+              label: '% of Marks',
+              data: percent_data,
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)'
+              ],
+              borderWidth: 2
+          }]
+      },
+      options: {
+          scales: {
+              y: {
+                  beginAtZero: true
+              }
+          }
+      }
+  });
+}
+
+chart_func(labels_data, percent_data);
 
