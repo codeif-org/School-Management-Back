a4c87fb019fe3e496857a6cf03f3bb0f431e771f
Mayur Gupta <mayurgupta7234@gmail.com>
2021-11-15 03:49:49 +0530

    fixed chart with both incomingData, updatedData

--- static/js/progressAPI.js
+++ static/js/progressAPI.js
@@ -1,5 +1,46 @@
 // console.log("progressAPI.js");
 
+// just pass labels and data to the chart_func
+let chart_func = function (labels_data, percent_data) {
+  var ctx = document.getElementById("myChart").getContext("2d");
+  var myChart = new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: labels_data,
+      datasets: [
+        {
+          label: "% of Marks",
+          data: percent_data,
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(255, 206, 86, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+            "rgba(255, 159, 64, 0.2)",
+          ],
+          borderColor: [
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+            "rgba(255, 159, 64, 1)",
+          ],
+          borderWidth: 2,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  });
+};
+
 // http://127.0.0.1:8000/exam/api/student/progress?subject=596
 
 // server side more vs client side more
@@ -15,13 +56,25 @@ let responseHandler = function (response) {
   var table_rows = document.getElementById("table-rows");
   //   console.log(table_rows);
   tableinnerHTML = "";
+  let labels_data_response = [];
+  let percent_data_response = [];
   data_arr.forEach((elem) => {
     tableinnerHTML += `<div class="table-list table-rows">
     <h3 class="left-column">${elem[1][0]}</h3>
     <h3 class="right-column">${elem[1][1]}/${elem[1][2]} = ${elem[1][3]}%</h3>
   </div>`;
+    labels_data_response.push(elem[1][0]);
+    percent_data_response.push(elem[1][3]);
   });
   table_rows.innerHTML = tableinnerHTML;
+
+  //   https://stackoverflow.com/questions/40056555/destroy-chart-js-bar-graph-to-redraw-other-graph-in-same-canvas
+  // destroy the chart before redrawing
+  let chartStatus = Chart.getChart("myChart"); // <canvas> id
+  if (chartStatus != undefined) {
+    chartStatus.destroy();
+  }
+  chart_func(labels_data_response, percent_data_response);
 };
 
 let submitHandler = function () {
@@ -67,9 +120,9 @@ console.log(Object.entries(mydata));
 
 // use eval to convert string to object
 // https://www.pluralsight.com/guides/convert-strings-to-json-objects-in-javascript-with-eval
-eval('('+mydata+')' );
-console.log(typeof eval('('+mydata+')' ));
-mydata = eval('('+mydata+')' );
+eval("(" + mydata + ")");
+console.log(typeof eval("(" + mydata + ")"));
+mydata = eval("(" + mydata + ")");
 console.log(mydata);
 console.log(typeof mydata);
 
@@ -77,52 +130,12 @@ let mydata_arr = Object.entries(mydata);
 let labels_data = [];
 let percent_data = [];
 mydata_arr.forEach((elem) => {
-    // console.log(elem);
-    labels_data.push(elem[1][0]);
-    percent_data.push(elem[1][3]);
-})
+  // console.log(elem);
+  labels_data.push(elem[1][0]);
+  percent_data.push(elem[1][3]);
+});
 
 console.log(labels_data);
 console.log(percent_data);
 
-
-let chart_func = function (labels_data, percent_data) {
-var ctx = document.getElementById('myChart').getContext('2d');
-  var myChart = new Chart(ctx, {
-      type: 'pie',
-      data: {
-          labels: labels_data,
-          datasets: [{
-              label: '% of Marks',
-              data: percent_data,
-              backgroundColor: [
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                  'rgba(255, 159, 64, 0.2)'
-              ],
-              borderColor: [
-                  'rgba(255, 99, 132, 1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)',
-                  'rgba(153, 102, 255, 1)',
-                  'rgba(255, 159, 64, 1)'
-              ],
-              borderWidth: 2
-          }]
-      },
-      options: {
-          scales: {
-              y: {
-                  beginAtZero: true
-              }
-          }
-      }
-  });
-}
-
 chart_func(labels_data, percent_data);
-
